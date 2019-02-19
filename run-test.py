d8a3e8406b257dee9955fa23a8b38c39782d8818
Aleksander Alekseev <mail@eax.me>
2019-02-19 17:15:05 +0300

    Jepsen tests (work in progress...)

--- run-test.py
+++ run-test.py
@@ -9,6 +9,7 @@ import tempfile
 START_PORT = 32000
 
 def run(cmd):
+    print("RUNNING: "+cmd)
     code = subprocess.call(cmd, shell=True)
     if code != 0:
         print("Command `%s` returned non-zero status: %d" %
@@ -20,16 +21,23 @@ def ssh(node, cmd):
         str(START_PORT + node)+""" gopher@localhost "bash -c 'source ./.bash_profile ; """+\
         cmd + """ '" 2>/dev/null""")
 
-def scp_to(node, lpath, rpath):
+def scp_to(node, lpath, rpath, flags=''):
     run("scp -o 'StrictHostKeyChecking no' -i ./ssh-keys/id_rsa -P"+\
-        str(START_PORT + node)+" " + lpath + " gopher@localhost:"+rpath+" 2>/dev/null")
+        str(START_PORT + node)+" "+flags+" " + lpath + " gopher@localhost:"+rpath+" 2>/dev/null")
 
-def scp_from(node, rpath, lpath):
+def scp_from(node, rpath, lpath, flags=''):
     run("scp -o 'StrictHostKeyChecking no' -i ./ssh-keys/id_rsa -P"+\
-        str(START_PORT + node)+" gopher@localhost:"+rpath+" "+lpath+" 2>/dev/null")
+        str(START_PORT + node)+" " + flags + " gopher@localhost:"+rpath+" "+lpath+" 2>/dev/null")
 
 
 ssh(1, 'echo "aaa" > test.txt')
 scp_from(1, "test.txt", "/tmp/test.txt")
 scp_to(1, "/tmp/test.txt", "test2.txt")
 ssh(1, 'cat test2.txt')
+
+run("rm -r /tmp/insolar-jepsen-data || true")
+scp_from(1, "go/src/github.com/insolar/insolar/data", "/tmp/insolar-jepsen-data", flags='-r')
+scp_to(2, "/tmp/insolar-jepsen-data", "go/src/github.com/insolar/insolar/data", flags='-r')
+scp_to(3, "/tmp/insolar-jepsen-data", "go/src/github.com/insolar/insolar/data", flags='-r')
+scp_to(4, "/tmp/insolar-jepsen-data", "go/src/github.com/insolar/insolar/data", flags='-r')
+scp_to(5, "/tmp/insolar-jepsen-data", "go/src/github.com/insolar/insolar/data", flags='-r')
