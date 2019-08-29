9eebcbb291b9edd7d4361a734f590c0c75d7a06d
Maria Zubareva <maria.zubareva@insolar.io>
2019-08-29 16:55:13 +0300

    NOISSUE: log to file

--- aggregate-logs.py
+++ aggregate-logs.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 
 START_PORT = 32001
-END_PORT = 32006
+END_PORT = 32007
 DEBUG = True
 
 def debug(msg):
@@ -30,4 +30,4 @@ for port in range(START_PORT, END_PORT+1):
     node_dir = copy_to_dir + str(port) + "/"
     run("""mkdir -p """+node_dir+""" || true """)
     run("""scp -o 'StrictHostKeyChecking no' -i ./base-image/id_rsa -P """+str(port)+\
-        """ gopher@localhost:go/src/github.com/insolar/insolar/*.log.gz """+node_dir+""" 2>/dev/null """)
+        """ gopher@localhost:go/src/github.com/insolar/insolar/*.log """+node_dir+""" 2>/dev/null """)
