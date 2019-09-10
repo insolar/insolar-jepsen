f33a5bdc1c036c2e9870a659cec968ce8f26e69d
Denis Linnik <denis.linnik@ins.world>
2019-09-10 19:26:35 +0300

    INS-3070: check balance added

--- run-test.py
+++ run-test.py
@@ -405,7 +405,7 @@ def run_benchmark(pod_ips, api_pod=VIRTUALS[0], ssh_pod=1, extra_args=""):
     virtual_pod_name = 'jepsen-'+str(api_pod)
     port = VIRTUAL_START_PORT + api_pod
     out = ssh_output(ssh_pod, 'cd go/src/github.com/insolar/insolar && ' +
-                     'timelimit -s9 -t10 ' +  # timeout: 10 seconds
+                     'timelimit -s9 -t30 ' +  # timeout: 10 seconds
                      './bin/benchmark -c ' + str(C) + ' -r ' + str(R) + ' -a http://'+pod_ips[virtual_pod_name] +
                      ':'+str(port) + '/admin-api/rpc ' +
                      ' -p http://'+pod_ips[virtual_pod_name]+':'+str(port + 100)+'/api/rpc ' +
