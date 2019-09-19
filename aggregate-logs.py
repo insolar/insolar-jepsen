2b4cccb6061fa0fb194fce1dbdfe3920c53f643b
Maria Zubareva <maria.zubareva@insolar.io>
2019-09-19 16:08:23 +0300

    NOISSUE: remove unexested files

--- aggregate-logs.py
+++ aggregate-logs.py
@@ -48,12 +48,12 @@ hostname = k8s_hostname()
 
 print("Info: k8s hostname = "+hostname)
 
-run("""rm """+copy_to_dir+"""all_errors.log""")
-run("""rm """+copy_to_dir+"""filtered_errors.log""")
+run("""rm -f """+copy_to_dir+"""all_errors.log""")
+run("""rm -f """+copy_to_dir+"""filtered_errors.log""")
 
 for port in range(START_PORT, END_PORT+1):
     node_dir = copy_to_dir + str(port) + "/"
-    run("""rm -r """+node_dir)
+    run("""rm -rf """+node_dir)
     run("""mkdir -p """+node_dir+""" || true """)
     if port == START_PORT:
         run("""scp -o 'StrictHostKeyChecking no' -i ./base-image/id_rsa -P """+str(port) +
