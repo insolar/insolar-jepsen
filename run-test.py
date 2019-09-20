376240b80e1256c10cd8d6f65fdca9805dcc68e8
Maria Zubareva <maria.zubareva@insolar.io>
2019-09-20 12:37:52 +0300

    INS-3579: show error msg

--- run-test.py
+++ run-test.py
@@ -466,7 +466,10 @@ def run_benchmark(pod_ips, api_pod=VIRTUALS[0], ssh_pod=1, extra_args="", c=C, r
     if background:
         return True, out
 
-    if 'Failers: '+str(C*R) in out:
+    if extra_args != "":
+        return False, out
+
+    if 'Successes: '+str(C*R) in out:
         return True, out
     return False, out
 
