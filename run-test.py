9d9f09d9cb00f732ff551cdcdc96ce3f95eb80e6
Aleksander Alekseev <mail@eax.me>
2019-09-30 17:01:08 +0300

    Minor changes

--- run-test.py
+++ run-test.py
@@ -1246,6 +1246,7 @@ if args.launch_only:
     POD_NODES = k8s_get_pod_nodes()
     wait_until_ssh_is_up_on_pods()
     pod_ips = k8s_get_pod_ips()
+    kill(PULSAR, "pulsard")
     info("=== Launching pulsard... ===")
     start_pulsard(extra_args="-s pulsard")
     info("=== Launching insolar network... ===")
