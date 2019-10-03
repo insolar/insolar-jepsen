07a7865a83d7f895e8a2e89455154309f53cc06e
Aleksander Alekseev <mail@eax.me>
2019-10-03 13:43:45 +0300

    NOISSUE: display unparsable lines in format-trace-logs.py

--- format-trace-logs.py
+++ format-trace-logs.py
@@ -5,6 +5,9 @@ import json
 import sys
 
 for line in sys.stdin:
-    line = line.strip()
-    [fname, log] = line.split(":", maxsplit=1)
-    print(json.loads(log)["time"]+" "+fname+" "+log)
+    try:
+        line = line.strip()
+        [fname, log] = line.split(":", maxsplit=1)
+        print(json.loads(log)["time"]+" "+fname+" "+log)
+    except Exception as e:
+        print("FAILED TO PARSE: "+line+"\nERROR: "+str(e), file=sys.stderr)
