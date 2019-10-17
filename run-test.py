bf3c07a399b8cc66690da352e0accdd79b1c851d
Aleksander Alekseev <mail@eax.me>
2019-10-17 17:26:46 +0300

    INS-3736: tests for observer (work in progress...)

--- run-test.py
+++ run-test.py
@@ -704,7 +704,7 @@ def deploy_pulsar():
 # []
 def deploy_observer(observer_path):
     info("deploying PostgreSQL @ pod "+str(OBSERVER))
-    ssh(OBSERVER, """sudo bash -c \\"apt install -y postgresql-9.5 && echo -e listen_addresses = \\x27*\\x27 >> /etc/postgresql/9.5/main/postgresql.conf && echo host all all 0.0.0.0/0 md5 >> /etc/postgresql/9.5/main/pg_hba.conf && service postgresql start\\" """)
+    ssh(OBSERVER, """sudo bash -c \\"apt install -y postgresql-9.5 && echo -e listen_addresses = \\\\x27*\\\\x27 >> /etc/postgresql/9.5/main/postgresql.conf && echo host all all 0.0.0.0/0 md5 >> /etc/postgresql/9.5/main/pg_hba.conf && service postgresql start\\" """)
     ssh(OBSERVER, """echo -e \\"CREATE DATABASE observer; CREATE USER observer WITH PASSWORD \\x27observer\\x27; GRANT ALL ON DATABASE observer TO observer;\\" | sudo -u postgres psql""")
     scp_to(OBSERVER, "./observer_scheme.sql", "/tmp/observer_scheme.sql")
     ssh(OBSERVER, "PGPASSWORD=observer psql -hlocalhost observer observer < /tmp/observer_scheme.sql")
