50edb5d14fcb91c2755465d671117387b0c2c857
Aleksander Alekseev <mail@eax.me>
2019-10-17 18:49:50 +0300

    INS-3736: tests for observer (work in progress...)

--- run-test.py
+++ run-test.py
@@ -706,7 +706,7 @@ def deploy_observer(observer_path):
     info("deploying PostgreSQL @ pod "+str(OBSERVER))
     # The base64-encoded string is: listen_addresses = '*'
     # I got tired to fight with escaping quotes in bash...
-    ssh(OBSERVER, """sudo bash -c \\"apt install -y postgresql-9.5 && echo bGlzdGVuX2FkZHJlc3NlcyA9ICcqJwo= | base64 -d >> /etc/postgresql/9.5/main/postgresql.conf && echo host all all 0.0.0.0/0 md5 >> /etc/postgresql/9.5/main/pg_hba.conf && service postgresql start\\" """)
+    ssh(OBSERVER, """sudo bash -c \\"apt install -y postgresql-11 && echo bGlzdGVuX2FkZHJlc3NlcyA9ICcqJwo= | base64 -d >> /etc/postgresql/11.4/main/postgresql.conf && echo host all all 0.0.0.0/0 md5 >> /etc/postgresql/11.4/main/pg_hba.conf && service postgresql start\\" """)
     ssh(OBSERVER, """echo -e \\"CREATE DATABASE observer; CREATE USER observer WITH PASSWORD \\x27observer\\x27; GRANT ALL ON DATABASE observer TO observer;\\" | sudo -u postgres psql""")
     scp_to(OBSERVER, "./observer_scheme.sql", "/tmp/observer_scheme.sql")
     ssh(OBSERVER, "PGPASSWORD=observer psql -hlocalhost observer observer < /tmp/observer_scheme.sql")
