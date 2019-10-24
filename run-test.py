da9f349dc22f0773e398f0157f18b00c51f80870
Aleksander Alekseev <mail@eax.me>
2019-10-24 17:30:52 +0300

    Change observer build command

--- run-test.py
+++ run-test.py
@@ -750,7 +750,7 @@ def deploy_observer(path):
     scp_to(OBSERVER, path + "/observer", INSPATH +
            "/../observer", flags="-r", ignore_errors=True)
     ssh(OBSERVER, "cd "+INSPATH +
-        "/../observer && rm -rf vendor && make ensure && make observer && mkdir -p .artifacts")
+        "/../observer && rm -rf vendor && make all && mkdir -p .artifacts")
     scp_to(OBSERVER, "/tmp/insolar-jepsen-configs/observer.yaml",
            INSPATH+"/../observer/.artifacts/observer.yaml")
     ssh(OBSERVER, """tmux new-session -d -s observer \\"cd """+INSPATH +
